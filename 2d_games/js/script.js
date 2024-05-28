a71c1adbb58a7a1ee3ae71c284bea413901f29e4
UTakuto <taku.0307.ue@icloud.com>
2024-05-28 16:30:56 +0900

    correction

--- 2d_games/js/script.js
+++ 2d_games/js/script.js
@@ -7,12 +7,7 @@ console.log(player.getBoundingClientRect());
 
 document.addEventListener("keydown", (event) => {
     const distance = 64;
-    // if(){}
-    player.style.top = y + "px";
-    player.style.left = x + "px";
-});
 
-function moveSet(event) {
     switch (event.code) {
         case "ArrowLeft":
             x -= distance;
@@ -27,4 +22,24 @@ function moveSet(event) {
             y += distance;
             break;
     }
-}
+
+    player.style.top = y + "px";
+    player.style.left = x + "px";
+});
+
+// function moveSet(event) {
+//     switch (event.code) {
+//         case "ArrowLeft":
+//             x -= distance;
+//             break;
+//         case "ArrowUp":
+//             y -= distance;
+//             break;
+//         case "ArrowRight":
+//             x += distance;
+//             break;
+//         case "ArrowDown":
+//             y += distance;
+//             break;
+//     }
+// }
