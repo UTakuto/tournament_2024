de3ff5c0b36476d66cf93af34f0ccb31c6c5036b
UTakuto <taku.0307.ue@icloud.com>
2024-06-03 00:47:02 +0900

    player 行動範囲 指定

--- 2d_games/js/script.js
+++ 2d_games/js/script.js
@@ -1,30 +1,49 @@
 const player = document.querySelector(".player");
+const staff = document.querySelector(".staff");
 
-let y = 0;
 let x = 0;
-
-console.log(player.getBoundingClientRect());
+let y = 0;
 
 document.addEventListener("keydown", (event) => {
-    const distance = 64;
+    const currentTop = player.offsetTop;
+    const currentLeft = player.offsetLeft;
+    let distance = 64;
 
     switch (event.code) {
         case "ArrowLeft":
-            x -= distance;
-            break;
-        case "ArrowUp":
-            y -= distance;
-            break;
-        case "ArrowRight":
-            x += distance;
-            break;
-        case "ArrowDown":
-            y += distance;
-            break;
-    }
+            if (currentLeft - distance >= 0) {
+                x -= distance;
+                break;
+            } else {
+                break;
+            }
 
-    player.style.top = y + "px";
+        case "ArrowUp":
+            if (currentTop - distance >= 0) {
+                y -= distance;
+                break;
+            } else {
+                break;
+            }
+
+        case "ArrowRight":
+            if (currentLeft + distance < 1024) {
+                x += distance;
+                break;
+            } else {
+                break;
+            }
+
+        case "ArrowDown":
+            if (currentTop + distance < 768) {
+                y += distance;
+                break;
+            } else {
+                break;
+            }
+    }
     player.style.left = x + "px";
+    player.style.top = y + "px";
 });
 
 // function moveSet(event) {
